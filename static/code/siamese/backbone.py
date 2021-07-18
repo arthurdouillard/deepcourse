6adf65ae85c20e7e0cc1b1e46fd024617c834d70
Arthur Douillard <ar.douillard@gmail.com>
2021-07-18 19:03:12 +0200

    Update website

--- static/code/siamese/backbone.py
+++ static/code/siamese/backbone.py
@@ -3,21 +3,24 @@ class Backbone(nn.Module):
     super().__init__()
 
     self.convs = nn.Sequential(
-        ConvBlock(3, 16, 3),
+        ConvBlock(3, 16, 3, 1),
+        ConvBlock(16, 16, 3, 1),
         nn.MaxPool2d((2, 2)),
-        ConvBlock(16, 32, 3),
+        ConvBlock(16, 32, 3, 1),
+        ConvBlock(32, 32, 3, 1),
         nn.MaxPool2d((2, 2)),
-        ConvBlock(32, 64, 3),
+        ConvBlock(32, 64, 3, 1),
+        ConvBlock(64, 64, 3, 1),
         nn.MaxPool2d((2, 2)),
-        ConvBlock(64, 128, 3),
-        ConvBlock(128, 64, 3),
+        ConvBlock(64, 128, 3, 1),
+        ConvBlock(128, 128, 3, 1),
     )
 
     self.fc = nn.Sequential(
         nn.Dropout(0.3),
-        nn.Linear(1024, 512),
+        nn.Linear(512, 512),
         nn.Dropout(0.3),
-        nn.Linear(512, 50)
+        nn.Linear(512, 100)
     )
 
   def forward(self, x):
